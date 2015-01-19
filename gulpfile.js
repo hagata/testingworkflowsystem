6c68cc3104f854ce3d6decbd0d432718bacd6905
genki <genki@hagata.info>
2015-01-19 12:28:30 -0800

    adds coffee gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,11 @@
 //Use node JS require command
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
-
-gulp.task('log',function(){
-	gutil.log('utils are working');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
+var coffeeSources = ['components/coffee/*.coffee']
+gulp.task('coffee', function(){
+	gulp.src(coffeeSources)
+		.pipe(coffee({bare: true})
+			.on('error',gutil.log))
+		.pipe(gulp.dest('components/scripts'))
 });
